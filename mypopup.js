691e53eb179a4aaf35e7f4af09b526ee9e346134
ttymed <tyler.boright@gmail.com>
2016-06-01 22:24:53 +0800

    deleted node_modules from github repo, deleted .org file from repo and planned out options page in test descriptions.  next to implement the tests

--- mypopup.js
+++ mypopup.js
@@ -24,24 +24,29 @@ class Counter {
 window.test_counter = new Counter()
 
 window.onload = () => {
-    if (document.getElementById('counter') && window.test_counter) {
-        let counterDiv = document.getElementById('counter')
-        let body = document.getElementsByTagName('body')[0]
-        let button = document.createElement('button')
-        let button2 = document.createElement('button')
-        counterDiv.innerHTML = localStorage['counter']
-        button.innerHTML = 'This is the increase button'
-        button2.innerHTML = 'This is the decrease button'
-        button.addEventListener('click', (() => {
-            window.test_counter.increaseCounter()
-            counterDiv.innerHTML = localStorage['counter']
+    if (document.getElementsByClassName('options') && window.test_counter) {
+        let input = document.getElementsByName('chain-name')[0]
+        let saveButton = document.getElementsByName('chain-save')[0]
+        saveButton.addEventListener('click', (() => {
+            console.log(input.value)
         }))
-        button2.addEventListener('click', (() => {
-            window.test_counter.decreaseCounter()
-            counterDiv.innerHTML = localStorage['counter']
-        }))
-        body.appendChild(button)
-        body.appendChild(button2)
+        // let counterDiv = document.getElementById('counter')
+        // let body = document.getElementsByTagName('body')[0]
+        // let button = document.createElement('button')
+        // let button2 = document.createElement('button')
+        // counterDiv.innerHTML = localStorage['counter']
+        // button.innerHTML = 'This is the increase button'
+        // button2.innerHTML = 'This is the decrease button'
+        // button.addEventListener('click', (() => {
+        //     window.test_counter.increaseCounter()
+        //     counterDiv.innerHTML = localStorage['counter']
+        // }))
+        // button2.addEventListener('click', (() => {
+        //     window.test_counter.decreaseCounter()
+        //     counterDiv.innerHTML = localStorage['counter'] || 0
+        // }))
+        // body.appendChild(button)
+        // body.appendChild(button2)
     } else if (document.getElementsByClassName('notification')) {
         let message = ''
         message = `The counter is currently at: ${window.test_counter.returnCounter()}`
--- test.js
+++ test.js
@@ -1,10 +1,34 @@
 import test from 'ava'
 
-test('test what t is', t => {
-    t.pass()
+test('take input from an input element and map it to an object', t => {
+    
 })
 
-test('can display', t => {
-    const tyler = 'tyler'
-    t.is(tyler, 'tyler', 'tyler is not a sara')
+test('habit added to object' t => {
+    
 })
+
+test('habit deleted from object' t => {
+    
+})
+
+test('if task exists, do not add task to object', t => {
+    
+})
+
+test('if task exists, show dom element informing user that task can not be added', t => {
+    
+}) 
+
+test('if habit saved, show habit saved message', t => {
+    
+})
+
+test('if habit deleted, show habit deleted message', t => {
+    
+})
+
+// test('can display', t => {
+//     const tyler = 'tyler'
+//     t.is(tyler, 'tyler', 'tyler is not a sara')
+// })
